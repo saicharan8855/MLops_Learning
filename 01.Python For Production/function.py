b014ed7e28207a3136c60e62afb123ebe6cc133b
saicharan8855 <saicharan9948644390@gmail.com>
2026-06-01 11:12:12 +0530

    day 01 : venv setup and test scripts

---
+++ 01.Python For Production/function.py
@@ -0,0 +1,12 @@
+def addition(a , b):
+    return a + b
+
+def greet_user(name):
+    return f"Hello , {name}"
+
+def return_library_version(library , version):
+    return f"library {library} , version {version}"
+
+print(addition(5 , 10))
+print(greet_user("saicharan"))
+print(return_library_version("numpy" , "1.21.0"))
---
+++ 01.Python For Production/test_env.py
@@ -0,0 +1,8 @@
+import requests 
+import pandas
+import numpy
+
+print(f"request version : {requests.__version__}")
+print(f"pandas version : {pandas.__version__}")
+print(f"numpy version : {numpy.__version__}")
+print("enivironment is working fine")
